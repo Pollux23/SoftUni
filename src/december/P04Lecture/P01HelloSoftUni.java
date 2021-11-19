3930bbf433cc03f594b96b33e8bf4dd3276528fa
idefixxx <boutdat@gmail.com>
2021-11-19 22:06:08 +0200

    moved1

---
+++ src/december/P04Lecture/P01HelloSoftUni.java
@@ -0,0 +1,11 @@
+package december.P04Lecture;
+
+public class P01HelloSoftUni {
+    public static void main(String[] args) {
+
+        System.out.println("Hello SoftUni");
+
+
+
+    }
+}
---
+++ src/december/P04Lecture/P07ProjectsCreation.java
@@ -0,0 +1,18 @@
+package december.P04Lecture;
+
+import java.util.Scanner;
+
+public class P07ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+        int projectNum = Integer.parseInt(scanner.nextLine());
+        int allProjectsTime = projectNum * 3;
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.");
+
+
+
+
+    }
+}
