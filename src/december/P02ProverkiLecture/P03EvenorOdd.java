5ff917bf3de6ff2d12ad34624379cf0e8a27185d
idefixxx <boutdat@gmail.com>
2021-11-22 17:35:23 +0200

    2

--- src/december/P02ProverkiLecture/P03EvenorOdd.java
+++ src/december/P02ProverkiLecture/P03EvenorOdd.java
@@ -1,7 +1,18 @@
 package december.P02ProverkiLecture;
 
+import java.util.Scanner;
+
 public class P03EvenorOdd {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
 
 
 
