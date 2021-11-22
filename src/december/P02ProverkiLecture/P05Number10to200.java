930877ffdc391bb558b7e8da1cd268177fb25d64
idefixxx <boutdat@gmail.com>
2021-11-22 17:34:58 +0200

    2

---
+++ src/december/P02ProverkiLecture/P05Number10to200.java
@@ -0,0 +1,23 @@
+package december.P02ProverkiLecture;
+
+import java.util.Scanner;
+
+public class P05Number10to200 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inputNum = Integer.parseInt(scanner.nextLine());
+
+        if (inputNum < 100) {
+            System.out.println("Less than 100");
+        } else if (inputNum <= 200)
+            System.out.println("Between 100 and 200");
+        else
+            System.out.println("Greater than 200");
+
+    }
+    }
+
+
+
+
---
+++ src/december/P02ProverkiLecture/P07AreaofFigures.java
@@ -0,0 +1,13 @@
+package december.P02ProverkiLecture;
+
+import java.util.Scanner;
+
+public class P07AreaofFigures {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+}
