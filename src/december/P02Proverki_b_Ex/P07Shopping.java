bbc0985844bc5614a741afd55043f58019814589
idefixxx <boutdat@gmail.com>
2021-12-09 11:31:00 +0200

    4

---
+++ src/december/P02Proverki_b_Ex/P07Shopping.java
@@ -0,0 +1,8 @@
+package december.P02Proverki_b_Ex;
+
+public class P07Shopping {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/december/P02Proverki_b_Ex/P08LunchBreak.java
@@ -0,0 +1,8 @@
+package december.P02Proverki_b_Ex;
+
+public class P08LunchBreak {
+    public static void main(String[] args) {
+
+
+    }
+}
