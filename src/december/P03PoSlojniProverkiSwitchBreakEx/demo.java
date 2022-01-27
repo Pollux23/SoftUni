11f20cbaec229242e2d744a53200d85fa5eb1c21
idefixxx <boutdat@gmail.com>
2022-01-27 12:16:59 +0200

    10

--- src/december/P03PoSlojniProverkiSwitchBreakEx/demo.java
+++
@@ -1,9 +0,0 @@
-package december.P03PoSlojniProverkiSwitchBreakEx;
-
-public class demo {
-    public static void main(String[] args) {
-
-
-
-    }
-}
---
+++ src/december/P03PoSlojniProverkiSwitchBreakEx/demoPercentCalculations.java
@@ -0,0 +1,17 @@
+package december.P03PoSlojniProverkiSwitchBreakEx;
+
+public class demoPercentCalculations {
+    public static void main(String[] args) {
+
+// percent of something is calculated as follows:
+
+        double price = 100.0;
+
+        // 5 % discount example
+        price = price - price * 0.05;
+        //or
+        price = price * 0.95;
+
+        System.out.println(price);
+    }
+}
