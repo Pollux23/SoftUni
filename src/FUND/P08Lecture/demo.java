a11d6ed019887bcd545704e0dce6200d8688ee80
idefixxx <boutdat@gmail.com>
2022-05-28 16:23:33 +0300

    21

--- src/FUND/P08Lecture/demo.java
+++ src/FUND/P08Lecture/demo.java
@@ -1,12 +1,24 @@
 package FUND.P08Lecture;
 
+import java.math.BigDecimal;
+
 public class demo {
     public static void main(String[] args) {
 
-double d = 100000000000000000.0;  // 17 nuli !
+        double d = 100000000000000000.0;  // 17 nuli !
 
         System.out.println(d);
-      //  1.0E17 // 17 nuli !
+        //  1.0E17 // 17 nuli !
+
+        short minValue = Short.MIN_VALUE;
+        short maxValue = Short.MAX_VALUE;
+        byte minValue1 = Byte.MIN_VALUE;
+        byte maxValue1 = Byte.MAX_VALUE;
+        long minValue2 = Long.MIN_VALUE;
+        long maxValue2 = Long.MAX_VALUE;
+        int minValue3 = Integer.MIN_VALUE;
+        double maxValuee = Double.MAX_VALUE;
+        System.out.println( 8 % 2.5);
 
     }
 }
