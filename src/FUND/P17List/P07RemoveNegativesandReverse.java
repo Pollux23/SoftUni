16c1fa4ba968eb80c12c31297c37aa259306624d
idefixxx <boutdat@gmail.com>
2022-07-27 09:16:31 +0300

    object and classes 2

--- src/FUND/P17List/P07RemoveNegativesandReverse.java
+++ src/FUND/P17List/P07RemoveNegativesandReverse.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-public class P07RemoveNegativesandReverse {
+public class P07RemoveNegativesAndReverse {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
--- src/FUND/P22ObjectAndClassesEX/P02Articles.java
+++ src/FUND/P22ObjectAndClassesEX/P02Articles.java
@@ -1,5 +1,10 @@
 package FUND.P22ObjectAndClassesEX;
 
 public class P02Articles {
+    public static void main(String[] args) {
+        int a = 5;
+        var b = 6;
+        System.out.println(a * b);
+    }
 
 }
